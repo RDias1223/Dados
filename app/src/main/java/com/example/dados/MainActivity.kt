793f0c54c6aaeaac0138b123835be564c3f570e6
RDias1223 <rdrony43@gmail.com>
2021-10-27 09:54:00 +0100

    random numbers

--- app/src/main/java/com/example/dados/MainActivity.kt
+++ app/src/main/java/com/example/dados/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.dados
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import kotlin.random.Random
@@ -20,9 +21,19 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun girarDado() {
-        val resultText:TextView= findViewById(R.id.result_text)
-        val randomInt=Random.nextInt(65)+1
-        resultText.text=randomInt.toString()
+        val mostrarDados:ImageView= findViewById(R.id.Dados_imagens)
+
+        val randomInt=Random.nextInt(6)+1
+
+        val resultadoDrawable =when(randomInt){
+            1->R.drawable.dice_1
+            2->R.drawable.dice_2
+            3->R.drawable.dice_3
+            4->R.drawable.dice_4
+            5->R.drawable.dice_5
+            else->R.drawable.dice_6
+        }
+        mostrarDados.setImageResource(resultadoDrawable)
     }
 
 }
