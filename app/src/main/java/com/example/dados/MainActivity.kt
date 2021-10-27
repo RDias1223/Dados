8db9f073cbd4184a12b670fc65ddce8fb3acc333
RDias1223 <rdrony43@gmail.com>
2021-10-27 09:10:16 +0100

    random numbers

--- app/src/main/java/com/example/dados/MainActivity.kt
+++ app/src/main/java/com/example/dados/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.dados
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import kotlin.random.Random
+import kotlin.random.nextInt
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,8 +14,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val rollButton: Button= findViewById(R.id.roll_Button)
         rollButton.setOnClickListener{
-            Toast.makeText(this,"button clicked",Toast.LENGTH_SHORT).show()
+            girarDado()
         }
 
     }
+
+    private fun girarDado() {
+        val resultText:TextView= findViewById(R.id.result_text)
+        val randomInt=Random.nextInt(65)+1
+        resultText.text=randomInt.toString()
+    }
+
 }
